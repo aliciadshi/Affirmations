265e6012da7c61dad5735432202e8b82841c5d69
Alicia Shi <destinyshi@gmail.com>
2023-04-23 13:01:32 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,10 @@
+package com.example.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.affirmations.R
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
